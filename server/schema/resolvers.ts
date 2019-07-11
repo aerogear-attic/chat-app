2cfa9c244764d680881df0b53dc6ce0aaaf82b68
Michal Stokluska <mstoklus@redhat.com>
2019-07-11 12:08:08 +0100

    fix: testing resolvers

--- server/schema/resolvers.ts
+++ server/schema/resolvers.ts
@@ -1,18 +1,18 @@
-import { GraphQLDateTime } from 'graphql-iso-date'
-import { chats, messages } from '../db'
+import { GraphQLDateTime } from "graphql-iso-date";
+import { chats, messages } from "../db";
 
 const resolvers = {
-    Date: GraphQLDateTime,
-    Chat: {
-        lastMessage(chat: any){
-            return messages.find( m => m.id === chat.lastMessage)
-        },
-    },
-    Query: {
-        chats() {
-            return chats;
-        },
-    },
+  Date: GraphQLDateTime,
+  Chat: {
+    lastMessage(chat: any) {
+      return messages.find(m => m.id === chat.lastMessage);
+    }
+  },
+  Query: {
+    chats() {
+      return chats;
+    }
+  }
 };
 
 export default resolvers;
