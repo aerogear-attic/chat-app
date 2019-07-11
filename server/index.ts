f5be829952c6ee28f8decf0852ec5ce77f527849
Michal Stokluska <mstoklus@redhat.com>
2019-07-11 10:14:09 +0100

    Fix: Transition to GraphQL

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,23 @@
+import { ApolloServer, gql } from 'apollo-server-express';
+import bodyParser from 'body-parser';
 import express from "express";
 import cors from 'cors'
-import { chats } from "./db";
+import schema from './schema';
 
 const app = express();
 
 //middleware - cors to allow cross origin requests(not local host only)
 app.use(cors())
 
-app.get("/chats", (req, res) => {
-  res.json(chats);
+//middleware - adding parser to that parses incoming request to middleware
+app.use(bodyParser.json())
+
+//inject schema into new ApolloServer
+const server = new ApolloServer({ schema })
+
+server.applyMiddleware({
+  app,
+  path: '/graphql'
 });
 
 const port = process.env.PORT || 4000;
---
+++ server/schema/resolvers.ts
@@ -0,0 +1,18 @@
+import { GraphQLDateTime } from 'graphql-iso-date'
+import { chats, messages } from '../db'
+
+const resolvers = {
+    Date: GraphQLDateTime,
+    Chat: {
+        lastMessage(chat: any){
+            return messages.find( m => m.id === chat.lastMessage)
+        },
+    },
+    Query: {
+        chats() {
+            return chats;
+        },
+    },
+};
+
+export default resolvers;
