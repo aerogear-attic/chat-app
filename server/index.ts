d3394efb3149dfebde4f274c1b95804acabc9143
Michal Stokluska <mstoklus@redhat.com>
2019-07-12 11:30:35 +0100

    fix: removing unused gql import

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServer } from "apollo-server-express";
 import bodyParser from "body-parser";
 import express from "express";
 import cors from "cors";
