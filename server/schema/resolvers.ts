726c2df9403c288ad82cf26c596978834318e57f
Michal Stokluska <mstoklus@redhat.com>
2019-07-12 15:56:46 +0100

    fix: adding first mutation

--- server/schema/resolvers.ts
+++ server/schema/resolvers.ts
@@ -6,7 +6,11 @@ const resolvers = {
   Chat: {
     messages(chat: any) {
       return messages.filter(m => chat.messages.includes(m.id));
-    }
+    },
+    lastMessage(chat: any) {
+      const lastMessage = chat.messages[chat.messages.length - 1];
+      return messages.find(m => m.id === lastMessage);
+    },
   },
   Query: {
     chats() {
@@ -15,6 +19,44 @@ const resolvers = {
 
     chat(root: any, { chatId }: any) {
       return chats.find(c => c.id === chatId);
+    }
+  },
+
+  Mutation: {
+    addMessage(root: any, { chatId, content }: any) {
+      //find index of chat by chatId
+      const chatIndex = chats.findIndex(c => c.id === chatId);
+      console.log("index is " + chatIndex)
+
+
+      if (chatIndex === -1) return null;
+    
+      //pull chat from chats[index no]
+      const chat = chats[chatIndex];
+      
+      //fetching last message in chat
+      const lastMessageId = messages.length;
+
+      //creating new incoming message Id number which is increment of last message by 1
+      const messageId = String(Number(lastMessageId) + 1);
+      
+      //creating new incoming message
+      const message = {
+        id: messageId,
+        createdAt: new Date(),
+        content
+      };
+      
+      //adding new message to the array of messages in db
+      messages.push(message);
+      //pushing messageId to the array of chat.messages field
+      chat.messages.push(messageId);
+
+      //chat message will appear at the top of the chatslist component
+      chats.splice(chatIndex, 1);
+      chats.unshift(chat);
+      return message;
+      
     }
   }
 };
