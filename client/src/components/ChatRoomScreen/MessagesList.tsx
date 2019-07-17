6ea33d7c8b1eb5a1fe872ce332aa15d96b1498b5
piccolazucca <milena.zuccarelli@gmail.com>
2019-07-17 15:13:54 +0100

    Add tests

--- client/src/components/ChatRoomScreen/MessagesList.tsx
+++ client/src/components/ChatRoomScreen/MessagesList.tsx
@@ -73,12 +73,15 @@ const MessagesList: React.FC<MessagesListProps> = ({ messages }) => {
   return (
     <Container>
       {messages.map((message: any) => (
-        <MessageItem key={message.id}>
-          <Contents>{message.content}</Contents>
-          <Timestamp>{moment(message.createdAt).format('HH:mm')}</Timestamp>
+        <MessageItem data-testid='message-item' key={message.id}>
+          <Contents data-testid='message-content'>{message.content}</Contents>
+          <Timestamp data-testid='message-date'>
+            {moment(message.createdAt).format('HH:mm')}
+          </Timestamp>
         </MessageItem>
       ))}
     </Container>
+
   );
 };
 
