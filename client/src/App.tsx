685dd75eefc393a07810482e07a4730bc12b25c0
piccolazucca <milena.zuccarelli@gmail.com>
2019-07-17 11:23:54 +0100

    Add chat rooms

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Redirect, RouteComponentProps } from 'react-router-dom';
 import ChatListScreen from './components/ChatListScreen';
+import ChatRoomScreen from './components/ChatRoomScreen';
+import MyAnimatedSwitch from './components/AnimatedSwitch';
 
-const App: React.FC = () => (
-  <div>
-    <ChatListScreen />
-  </div>
-);
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <MyAnimatedSwitch>
+        <Route exact path='/chats' component={ChatListScreen} />
+
+        <Route
+          exact
+          path='/chats/:chatId'
+          component={({ match, history }: RouteComponentProps<{ chatId: string }>) => (
+            <ChatRoomScreen chatId={match.params.chatId} history={history} />
+          )} />
+      </MyAnimatedSwitch>
+      <Route exact path='/' render={redirectToChats} />
+    </BrowserRouter>
+  );
+};
+
+const redirectToChats = () => <Redirect to='/chats' />;
 
 export default App;
---
+++ client/src/components/ChatRoomScreen/index.tsx
@@ -0,0 +1,101 @@
+import React, { useCallback, useState, useMemo } from 'react';
+import styled from 'styled-components';
+import ChatNavbar from './ChatNavbar';
+import MessageInput from './MessageInput';
+import MessagesList from './MessagesList';
+import { History } from 'history';
+const Container = styled.div`
+  background: url(/assets/chat-background.jpg);
+  display: flex;
+  flex-flow: column;
+  height: 100vh;
+`;
+
+const getChatQuery = `
+  query GetChat($chatId: ID!) {
+    chat(chatId: $chatId) {
+      id
+      name
+      picture
+      messages {
+        id
+        content
+        createdAt
+      }
+    }
+  }
+`;
+
+interface ChatRoomScreenParams {
+  chatId: string;
+  history: History;
+}
+
+export interface ChatQueryMessage {
+  id: string;
+  content: string;
+  createdAt: number;
+}
+
+export interface ChatQueryResult {
+  id: string;
+  name: string;
+  picture: string;
+  messages: ChatQueryMessage[];
+}
+
+type OptionalChatQueryResult = ChatQueryResult | null;
+
+const ChatRoomScreen: React.FC<ChatRoomScreenParams> = ({ history, chatId }) => {
+  const [chat, setChat] = useState<OptionalChatQueryResult>(null);
+
+  useMemo(async () => {
+    const body = await fetch(`${process.env.REACT_APP_SERVER_URL}/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        query: getChatQuery,
+        variables: { chatId }
+      })
+    });
+    const { data: { chat } } = await body.json();
+    setChat(chat);
+  }, [chatId]);
+
+  const onSendMessage = useCallback(
+    (content: string) => {
+      if (!chat) {
+        return null;
+      }
+
+      const message = {
+        id: (chat.messages.length + 10).toString(),
+        createdAt: Date.now(),
+        content
+      };
+
+      console.log(chat.messages);
+
+      setChat({
+        ...chat,
+        messages: chat.messages.concat(message)
+      });
+    }, [chat]
+  );
+
+  if (!chat) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <ChatNavbar chat={chat} history={history} />
+      {chat.messages && <MessagesList messages={chat.messages} />}
+      <MessageInput onSendMessage={onSendMessage} />
+    </Container>
+  );
+};
+
+export default ChatRoomScreen;
