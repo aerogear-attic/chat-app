6b70d0c8cbe56c08abab71f070a5e1abc1f3ecfe
Michal Stokluska <mstoklus@redhat.com>
2019-07-18 14:58:44 +0100

    feat: Get user from auth token

--- server/context.ts
+++ server/context.ts
@@ -1,9 +1,11 @@
 import { PubSub } from "apollo-server-express";
 import { User } from "./db";
+import { Response } from "express";
 
 // creating context that contains both pubsub & current user types that were applied to apollo server in index.ts
 
 export type MyContext = {
   pubsub: PubSub;
   currentUser: User;
+  res: Response;
 };
