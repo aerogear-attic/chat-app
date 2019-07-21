489833b24cb2161eae2703ac931e95940a9782d1
piccolazucca <milena.zuccarelli@gmail.com>
2019-07-21 22:34:40 +0100

    Provide Apollo client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,22 +1,26 @@
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo-hooks';
 import './index.css';
 import App from './App';
+import client from './client';
 import * as serviceWorker from './serviceWorker';
 
 const theme = createMuiTheme({
-    palette: {
-        primary: { main: '#2c6157' },
-        secondary: { main: '#6fd056' }
-    }
+  palette: {
+    primary: { main: '#2c6157' },
+    secondary: { main: '#6fd056' }
+  }
 });
 
 ReactDOM.render(
-    <MuiThemeProvider theme={theme}>
-        <App />
-    </MuiThemeProvider>,
-    document.getElementById('root')
+  <MuiThemeProvider theme={theme}>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </MuiThemeProvider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
