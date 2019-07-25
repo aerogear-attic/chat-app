cbec1b7d91f518a840930a454b4752fee7143b4a
Michal Stokluska <mstoklus@redhat.com>
2019-07-25 14:23:07 +0100

    refactor: resolvers modularity added

--- server/schema/index.ts
+++ server/schema/index.ts
@@ -1,11 +1,17 @@
-import { importSchema } from "graphql-import";
 // importing IResolvers to make sure resolvers handlers have right signatures - (obj, args, context, info)
 import { makeExecutableSchema, IResolvers } from "graphql-tools";
-import resolvers from "./resolvers";
 
-const typeDefs = importSchema("schema/typeDefs.graphql");
+import { merge } from "lodash";
+import * as commonModule from "../modules/common";
+import * as usersModule from "../modules/users";
+import * as chatsModule from "../modules/chats";
 
 export default makeExecutableSchema({
-  resolvers: resolvers as IResolvers,
-  typeDefs
+  resolvers: merge(
+    {},
+    commonModule.resolvers,
+    usersModule.resolvers,
+    chatsModule.resolvers
+  ) as IResolvers,
+  typeDefs: [commonModule.typeDefs, usersModule.typeDefs, chatsModule.typeDefs]
 });
