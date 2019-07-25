33db27e3d3403a74cdabd6ea08c89ca5594b9389
Michal Stokluska <mstoklus@redhat.com>
2019-07-25 08:45:47 +0100

    style: minor styling fixes in .db

--- server/db.ts
+++ server/db.ts
@@ -1,6 +1,6 @@
-import { Pool } from 'pg';
-import sql from 'sql-template-strings';
-import { resetDb as envResetDb } from './env';
+import { Pool } from "pg";
+import sql from "sql-template-strings";
+import { resetDb as envResetDb } from "./env";
 
 export type User = {
   id: string;
@@ -23,11 +23,11 @@ export type Chat = {
 };
 
 export const dbConfig = {
-  host: 'localhost',
+  host: "localhost",
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  user: 'testuser',
-  password: 'testpassword',
-  database: 'whatsapp',
+  user: "testuser",
+  password: "testpassword",
+  database: "whatsapp"
 };
 
 export let pool: Pool = new Pool(dbConfig);
@@ -74,48 +74,46 @@ export const resetDb = async () => {
 
   const sampleUsers = [
     {
-      id: '1',
-      name: 'Ray Edwards',
-      username: 'ray',
-      password: '$2a$08$NO9tkFLCoSqX1c5wk3s7z.JfxaVMKA.m7zUDdDwEquo4rvzimQeJm', // 111
-      picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
+      id: "1",
+      name: "Ray Edwards",
+      username: "ray",
+      password: "$2a$08$NO9tkFLCoSqX1c5wk3s7z.JfxaVMKA.m7zUDdDwEquo4rvzimQeJm", // 111
+      picture: "https://randomuser.me/api/portraits/thumb/lego/1.jpg"
     },
     {
-      id: '2',
-      name: 'Ethan Gonzalez',
-      username: 'ethan',
-      password: '$2a$08$xE4FuCi/ifxjL2S8CzKAmuKLwv18ktksSN.F3XYEnpmcKtpbpeZgO', // 222
-      picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
+      id: "2",
+      name: "Ethan Gonzalez",
+      username: "ethan",
+      password: "$2a$08$xE4FuCi/ifxjL2S8CzKAmuKLwv18ktksSN.F3XYEnpmcKtpbpeZgO", // 222
+      picture: "https://randomuser.me/api/portraits/thumb/men/1.jpg"
     },
     {
-      id: '3',
-      name: 'Bryan Wallace',
-      username: 'bryan',
-      password: '$2a$08$UHgH7J8G6z1mGQn2qx2kdeWv0jvgHItyAsL9hpEUI3KJmhVW5Q1d.', // 333
-      picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
+      id: "3",
+      name: "Bryan Wallace",
+      username: "bryan",
+      password: "$2a$08$UHgH7J8G6z1mGQn2qx2kdeWv0jvgHItyAsL9hpEUI3KJmhVW5Q1d.", // 333
+      picture: "https://randomuser.me/api/portraits/thumb/men/2.jpg"
     },
     {
-      id: '4',
-      name: 'Avery Stewart',
-      username: 'avery',
-      password: '$2a$08$wR1k5Q3T9FC7fUgB7Gdb9Os/GV7dGBBf4PLlWT7HERMFhmFDt47xi', // 444
-      picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
+      id: "4",
+      name: "Avery Stewart",
+      username: "avery",
+      password: "$2a$08$wR1k5Q3T9FC7fUgB7Gdb9Os/GV7dGBBf4PLlWT7HERMFhmFDt47xi", // 444
+      picture: "https://randomuser.me/api/portraits/thumb/women/1.jpg"
     },
     {
-      id: '5',
-      name: 'Katie Peterson',
-      username: 'katie',
-      password: '$2a$08$6.mbXqsDX82ZZ7q5d8Osb..JrGSsNp4R3IKj7mxgF6YGT0OmMw242', // 555
-      picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
-    },
+      id: "5",
+      name: "Katie Peterson",
+      username: "katie",
+      password: "$2a$08$6.mbXqsDX82ZZ7q5d8Osb..JrGSsNp4R3IKj7mxgF6YGT0OmMw242", // 555
+      picture: "https://randomuser.me/api/portraits/thumb/women/2.jpg"
+    }
   ];
 
   for (const sampleUser of sampleUsers) {
     await pool.query(sql`
       INSERT INTO users(id, name, username, password, picture)
-      VALUES(${sampleUser.id}, ${sampleUser.name}, ${sampleUser.username}, ${
-      sampleUser.password
-    }, ${sampleUser.picture})
+      VALUES(${sampleUser.id}, ${sampleUser.name}, ${sampleUser.username}, ${sampleUser.password}, ${sampleUser.picture})
     `);
   }
 
@@ -131,17 +129,17 @@ export const resetDb = async () => {
 
   const sampleChats = [
     {
-      id: '1',
+      id: "1"
     },
     {
-      id: '2',
+      id: "2"
     },
     {
-      id: '3',
+      id: "3"
     },
     {
-      id: '4',
-    },
+      id: "4"
+    }
   ];
 
   for (const sampleChat of sampleChats) {
@@ -159,37 +157,37 @@ export const resetDb = async () => {
 
   const sampleChatsUsers = [
     {
-      chat_id: '1',
-      user_id: '1',
+      chat_id: "1",
+      user_id: "1"
     },
     {
-      chat_id: '1',
-      user_id: '2',
+      chat_id: "1",
+      user_id: "2"
     },
     {
-      chat_id: '2',
-      user_id: '1',
+      chat_id: "2",
+      user_id: "1"
     },
     {
-      chat_id: '2',
-      user_id: '3',
+      chat_id: "2",
+      user_id: "3"
     },
     {
-      chat_id: '3',
-      user_id: '1',
+      chat_id: "3",
+      user_id: "1"
     },
     {
-      chat_id: '3',
-      user_id: '4',
+      chat_id: "3",
+      user_id: "4"
     },
     {
-      chat_id: '4',
-      user_id: '1',
+      chat_id: "4",
+      user_id: "1"
     },
     {
-      chat_id: '4',
-      user_id: '5',
-    },
+      chat_id: "4",
+      user_id: "5"
+    }
   ];
 
   for (const sampleChatUser of sampleChatsUsers) {
@@ -201,45 +199,43 @@ export const resetDb = async () => {
 
   await pool.query(sql`DELETE FROM messages`);
 
-  const baseTime = new Date('1 Jan 2019 GMT').getTime();
+  const baseTime = new Date("1 Jan 2019 GMT").getTime();
 
   const sampleMessages = [
     {
-      id: '1',
-      content: 'You on your way?',
+      id: "1",
+      content: "You on your way?",
       created_at: new Date(baseTime - 60 * 1000 * 1000),
-      chat_id: '1',
-      sender_user_id: '1',
+      chat_id: "1",
+      sender_user_id: "1"
     },
     {
-      id: '2',
+      id: "2",
       content: "Hey, it's me",
       created_at: new Date(baseTime - 2 * 60 * 1000 * 1000),
-      chat_id: '2',
-      sender_user_id: '1',
+      chat_id: "2",
+      sender_user_id: "1"
     },
     {
-      id: '3',
-      content: 'I should buy a boat',
+      id: "3",
+      content: "I should buy a boat",
       created_at: new Date(baseTime - 24 * 60 * 1000 * 1000),
-      chat_id: '3',
-      sender_user_id: '1',
+      chat_id: "3",
+      sender_user_id: "1"
     },
     {
-      id: '4',
-      content: 'This is wicked good ice cream.',
+      id: "4",
+      content: "This is wicked good ice cream.",
       created_at: new Date(baseTime - 14 * 24 * 60 * 1000 * 1000),
-      chat_id: '4',
-      sender_user_id: '1',
-    },
+      chat_id: "4",
+      sender_user_id: "1"
+    }
   ];
 
   for (const sampleMessage of sampleMessages) {
     await pool.query(sql`
       INSERT INTO messages(id, content, created_at, chat_id, sender_user_id)
-      VALUES(${sampleMessage.id}, ${sampleMessage.content}, ${
-      sampleMessage.created_at
-    }, ${sampleMessage.chat_id}, ${sampleMessage.sender_user_id})
+      VALUES(${sampleMessage.id}, ${sampleMessage.content}, ${sampleMessage.created_at}, ${sampleMessage.chat_id}, ${sampleMessage.sender_user_id})
     `);
   }
 
