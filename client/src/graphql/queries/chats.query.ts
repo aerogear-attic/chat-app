642b5323290adcc2ae02cbe574106088b1687a3a
piccolazucca <milena.zuccarelli@gmail.com>
2019-07-28 20:04:46 +0100

    fix: rewrite lastMessage to chats query

---
+++ client/src/graphql/queries/chats.query.ts
@@ -0,0 +1,15 @@
+import gql from 'graphql-tag';
+export default gql`
+  query Chats {
+    chats {
+      id
+      name
+      picture
+      lastMessage {
+        id
+        content
+        createdAt
+      }
+    }
+  }
+`;
