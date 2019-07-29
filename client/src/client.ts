168930a438b63420b666048d1b2c85fb7c1f9b06
piccolazucca <milena.zuccarelli@gmail.com>
2019-07-29 17:16:11 +0100

    feat: set up web socket link

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,16 +1,38 @@
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
+import { getMainDefinition } from 'apollo-utilities';
 import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { ApolloLink, split } from 'apollo-link';
 
 const httpUri = process.env.REACT_APP_SERVER_URL + '/graphql';
+const wsUri = httpUri.replace(/^https?/, 'ws');
 
 const httpLink = new HttpLink({
   uri: httpUri
 });
 
+const wsLink = new WebSocketLink({
+  uri: wsUri,
+  options: {
+    reconnect: true
+  }
+});
+
+const terminatingLink = split(
+  ({query}) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink
+);
+
+const link = ApolloLink.from([terminatingLink]);
+
 const inMemoryCache = new InMemoryCache();
 
 export default new ApolloClient({
-  link: httpLink,
+  link,
   cache: inMemoryCache
 });
