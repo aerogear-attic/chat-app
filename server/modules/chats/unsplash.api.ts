b41a95011c526e4876f9ae1161449c2c25d3f9ea
Michal Stokluska <mstoklus@redhat.com>
2019-07-30 12:24:10 +0100

    fix: issues in unsplash and tests fixed

--- server/modules/chats/unsplash.api.ts
+++ server/modules/chats/unsplash.api.ts
@@ -1,11 +1,8 @@
-import { RESTDataSource, RequestOptions } from "apollo-datasource-rest";
+import { Injectable, ProviderScope } from "@graphql-modules/di";
 import { resolve } from "path";
+import axios from "axios";
 import { trackProvider } from "@safe-api/middleware";
 import { RandomPhoto } from "../../types/unsplash";
-import { Injectable, ProviderScope } from "@graphql-modules/di";
-
-// using rest data resource to be able to use http methods from node amd safe api tracker
-// to ensure that data received from unsplashed is correct
 
 interface RandomPhotoInput {
   query: string;
@@ -15,27 +12,33 @@ interface RandomPhotoInput {
 @Injectable({
   scope: ProviderScope.Application
 })
-export class UnsplashApi extends RESTDataSource {
-  constructor() {
-    super();
-    this.baseURL = "https://api.unsplash.com/";
-  }
-  willSendRequest(request: RequestOptions) {
-    request.headers.set(
-      "Authorization",
-      "Client-ID 4d048cfb4383b407eff92e4a2a5ec36c0a866be85e64caafa588c110efad350d"
-    );
-  }
+export class UnsplashApi {
+  baseURL = "https://api.unsplash.com/";
+
   async getRandomPhoto() {
     const trackedRandomPhoto = await trackProvider(
-      ({ query, orientation }: RandomPhotoInput) =>
-        this.get<RandomPhoto>("photos/random", { query, orientation }),
+      async ({ query, orientation }: RandomPhotoInput) => {
+        const response = await axios.get<RandomPhoto>("photos/random", {
+          baseURL: this.baseURL,
+          headers: {
+            Authorization:
+              "Client-ID 4d048cfb4383b407eff92e4a2a5ec36c0a866be85e64caafa588c110efad350d"
+          },
+          params: {
+            query,
+            orientation
+          }
+        });
+
+        return response.data;
+      },
       {
         provider: "Unsplash",
         method: "RandomPhoto",
         location: resolve(__dirname, "../logs/main")
       }
     );
+
     try {
       return (await trackedRandomPhoto({
         query: "portrait",
