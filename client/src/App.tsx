90ffd374e0805ce1572ad06b69557b2846342200
piccolazucca <milena.zuccarelli@gmail.com>
2019-07-31 15:46:34 +0100

    feat: add auth screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, RouteComponentProps } from 'react-router-dom';
+import AuthScreen from './components/AuthScreen';
 import ChatListScreen from './components/ChatListScreen';
 import ChatRoomScreen from './components/ChatRoomScreen';
 import MyAnimatedSwitch from './components/AnimatedSwitch';
-import { useCacheService } from './services/cache.service';
+import { withAuth } from './services/auth.service';
 
 const App: React.FC = () => {
-  useCacheService();
-
   return (
     <BrowserRouter>
       <MyAnimatedSwitch>
+        <Route exact path='/sign-(in|up)' component={AuthScreen} />
         <Route exact path='/chats' component={ChatListScreen} />
 
         <Route
           exact
           path='/chats/:chatId'
-          component={({ match, history }: RouteComponentProps<{ chatId: string }>) => (
-            <ChatRoomScreen chatId={match.params.chatId} history={history} />
-          )} />
+          component={withAuth(
+            ({ match, history }: RouteComponentProps<{ chatId: string }>) => (
+              <ChatRoomScreen chatId={match.params.chatId} history={history} />
+            )
+          )}
+        />
       </MyAnimatedSwitch>
       <Route exact path='/' render={redirectToChats} />
     </BrowserRouter>
---
+++ client/src/services/auth.service.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useCallback } from 'react';
+import { useApolloClient } from 'react-apollo-hooks';
+import { Redirect } from 'react-router-dom';
+import { useCacheService } from './cache.service';
+
+export const withAuth = <P extends object>(
+  Component: React.ComponentType<P>
+) => {
+  return (props: any) => {
+    if (!isSignedIn()) {
+      if (props.history.location.pathname === '/sign-in') {
+        return null;
+      }
+
+      return <Redirect to='/sign-in' />;
+    }
+
+    useCacheService();
+
+    return <Component {...props as P} />;
+  };
+};
+
+export const signIn = (currentUserId: string) => {
+  document.cookie = `currentUserId=${currentUserId}`;
+
+  // This will become async in the near future
+  return Promise.resolve();
+};
+
+export const useSignOut = () => {
+  const client = useApolloClient();
+
+  return useCallback(() => {
+    // "expires" represents the lifespan of a cookie. Beyond that date the cookie will
+    // be deleted by the browser. "expires" cannot be viewed from "document.cookie"
+    document.cookie = `currentUserId=;expires=${new Date(0)}`;
+
+    // Clear cache
+    return client.clearStore();
+  }, [client]);
+};
+
+export const isSignedIn = () => {
+  return /currentUserId=.+(;|$)/.test(document.cookie);
+};
