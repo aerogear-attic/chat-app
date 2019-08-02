30be4f195297ce7ffe0dcc7a08a20695adc182b5
Michal Stokluska <mstoklus@redhat.com>
2019-08-02 15:19:42 +0100

    feat: add MQTT broker

--- server/modules/common/pubsub.provider.ts
+++ server/modules/common/pubsub.provider.ts
@@ -1,1 +1,29 @@
+import { PubSub } from "apollo-server-express";
+import mqtt, { IClientOptions } from "mqtt";
+import { MQTTPubSub } from "@aerogear/graphql-mqtt-subscriptions";
+
+const mqttHost = process.env.MQTT_HOST || "localhost";
+
+console.log("Using MQTT PubSub");
+const mqttOptions = {
+  host: mqttHost,
+  servername: mqttHost, // needed to work in OpenShift. Lookup SNI.
+  username: process.env.MQTT_USERNAME || "localhost",
+  password: process.env.MQTT_PASSWORD || "localhost",
+  port: process.env.MQTT_PORT || 1883,
+  protocol: process.env.MQTT_PROTOCOL || "mqtt",
+  rejectUnauthorized: false
+};
+
+const client = mqtt.connect(mqttHost, mqttOptions as IClientOptions) as any;
+
+console.log(`attempting to connect to messaging service ${mqttHost}`);
+
+client.on("connect", () => {
+  console.log("connected to messaging service");
+});
+
+const pubsub = new MQTTPubSub({ client });
+
+export { pubsub };
 export { PubSub } from "apollo-server-express";
