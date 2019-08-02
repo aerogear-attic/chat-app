bdd3d72676de6152668f67803a9ae24ba24a7e85
Michal Stokluska <mstoklus@redhat.com>
2019-08-02 15:19:42 +0100

    feat: add MQTT broker

--- server/modules/common/index.ts
+++ server/modules/common/index.ts
@@ -6,11 +6,11 @@ import { pool } from "../../db";
 import { Pool } from "pg";
 import { Database } from "./database.provider";
 import { ProviderScope } from "@graphql-modules/di";
-import { PubSub } from "./pubsub.provider";
+import { PubSub, pubsub } from "./pubsub.provider";
 
 // Implementation of postgres notify/listen for PubSub mechanism, as apollo pubsub is not for production usage
 // require instead of import from as graphql-postgres-subscription does not support typeScript
-const { PostgresPubSub } = require("graphql-postgres-subscriptions");
+// const { PostgresPubSub } = require("graphql-postgres-subscriptions");
 
 const typeDefs = gql`
   scalar DateTime
@@ -27,15 +27,6 @@ const typeDefs = gql`
 const resolvers: Resolvers = {
   DateTime: GraphQLDateTime
 };
-
-// creating postgres pubsub event listener
-const pubsub = new PostgresPubSub({
-  host: "localhost",
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  user: "testuser",
-  password: "testpassword",
-  database: "whatsapp"
-});
 
 export default new GraphQLModule({
   name: "common",
