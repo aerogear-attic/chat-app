a88cafcfbd7d6b2b82f0f44451e81d1adf3c4981
Michal Stokluska <mstoklus@redhat.com>
2019-08-08 15:23:39 +0100

    fix: dockerfile changes

---
+++ scripts/buildPublishContainer.sh
@@ -0,0 +1,16 @@
+[ -z "$CI" ] && echo "This script is meant to run only from CircleCI." && exit 1;
+[ -z "$FOLDER" ] && echo "Missing folder env variable - should be server or client" && exit 1;
+#[ -z "$DOCKERHUB_USERNAME" ] && echo "Undefined DOCKERHUB_USERNAME, skipping publish" && exit 1;
+
+#docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+
+#use TAG env. variable to create the container with the given tag
+TAG="${TAG:-latest}"
+
+APPNAME="chatapp-$FOLDER"
+NAMESPACE="aerogear"
+CONTAINER="$NAMESPACE/$APPNAME:$TAG"
+
+echo "Building docker container $CONTAINER"
+docker build -f $FOLDER/Dockerfile -t "$CONTAINER" .
+#docker push "$CONTAINER"
