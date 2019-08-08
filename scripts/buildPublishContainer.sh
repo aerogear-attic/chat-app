a312c0548f304498d26d97c0b2137c0cdd63d6dd
Michal Stokluska <m.stokluska@ymail.com>
2019-08-08 15:44:35 +0100

    fix scripts

--- scripts/buildPublishContainer.sh
+++ scripts/buildPublishContainer.sh
@@ -1,8 +1,8 @@
 [ -z "$CI" ] && echo "This script is meant to run only from CircleCI." && exit 1;
 [ -z "$FOLDER" ] && echo "Missing folder env variable - should be server or client" && exit 1;
-#[ -z "$DOCKERHUB_USERNAME" ] && echo "Undefined DOCKERHUB_USERNAME, skipping publish" && exit 1;
+[ -z "$DOCKERHUB_USERNAME" ] && echo "Undefined DOCKERHUB_USERNAME, skipping publish" && exit 1;
 
-#docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
 
 #use TAG env. variable to create the container with the given tag
 TAG="${TAG:-latest}"
@@ -13,4 +13,4 @@ CONTAINER="$NAMESPACE/$APPNAME:$TAG"
 
 echo "Building docker container $CONTAINER"
 docker build -f $FOLDER/Dockerfile -t "$CONTAINER" .
-#docker push "$CONTAINER"
+docker push "$CONTAINER"
