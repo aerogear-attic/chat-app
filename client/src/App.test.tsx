df31db635e4b91d2c18b5577458a18aae08de658
Dara Hayes <dara.hayes@redhat.com>
2019-08-09 11:41:12 +0100

    fix: failing test App.test.tsx

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { ApolloProvider } from 'react-apollo-hooks';
+import { OffixProvider } from 'react-offix-hooks';
+import { createOfflineClient } from './client'
 import ReactDOM from 'react-dom';
 import App from './App';
 import { mockApolloClient } from './test-helpers';
 import * as subscriptions from './graphql/subscriptions';
 
 it('renders without crashing', () => {
-  const client = mockApolloClient([
-    {
-      request: { query: subscriptions.messageAdded },
-      result: { data: {} }
-    }
-  ]);
+  const client = createOfflineClient()
+
   const div = document.createElement('div');
 
   ReactDOM.render(
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>,
+    <>
+      {/*
+      // @ts-ignore */}
+      <OffixProvider client={client}>
+        <App />
+      </OffixProvider>
+    </>,
     div
   );
   ReactDOM.unmountComponentAtNode(div);
