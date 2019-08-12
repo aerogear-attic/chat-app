38770259e2f95248b768b7d9e2b761b197dacd38
Dara Hayes <dara.hayes@redhat.com>
2019-08-12 10:44:29 +0100

    fix: passing server url to client builds

--- scripts/buildPublishContainer.sh
+++ scripts/buildPublishContainer.sh
@@ -1,5 +1,5 @@
 [ -z "$CI" ] && echo "This script is meant to run only from CircleCI." && exit 1;
-[ -z "$FOLDER" ] && echo "Missing folder env variable - should be server or client" && exit 1;
+[ -z "$FOLDER" ] && echo "Missing FOLDER env variable - should be server or client" && exit 1;
 [ -z "$DOCKERHUB_USERNAME" ] && echo "Undefined DOCKERHUB_USERNAME, skipping publish" && exit 1;
 
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
@@ -12,5 +12,12 @@ NAMESPACE="aerogear"
 CONTAINER="$NAMESPACE/$APPNAME:$TAG"
 
 echo "Building docker container $CONTAINER"
-docker build -f $FOLDER/Dockerfile -t "$CONTAINER" .
+
+if [ "$FOLDER" == "client" ]; then
+  [ -z "$SERVER_URL" ] && echo "Missing SERVER_URL env variable" && exit 1;
+  docker build -f $FOLDER/Dockerfile -t "$CONTAINER" --build-arg server_url=$SERVER_URL .
+else
+  docker build -f $FOLDER/Dockerfile -t "$CONTAINER" .
+fi
+
 docker push "$CONTAINER"
