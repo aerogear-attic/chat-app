c01d9288bdd84ed185f6b2a9678fa608c4e10d07
Dara Hayes <dara.hayes@redhat.com>
2019-08-14 16:16:01 +0100

    fix: middleware ordering

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,8 @@ import { app } from "./app";
 import { origin, port } from "./env";
 import http from "http";
 import { server } from "./server";
+import express from 'express'
+import path from 'path'
 
 // enabling server to receive and set cookies and use of credentials sent in http get header
 server.applyMiddleware({
@@ -15,6 +17,12 @@ const httpServer = http.createServer(app);
 
 // installing subscription handlers on httpServer
 server.installSubscriptionHandlers(httpServer);
+
+app.use(express.static(path.join(__dirname, '/../client/build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 httpServer.listen(port, () => {
   console.log(`Server is running on port ${port}`);
